f2e8d4036b1fec58ccae828be4cadb64f3e27d2d
Brett Henderson <BrettH@m2north.com>
2025-01-24 15:26:49 +0000

    fix: issues on examples page

--- src/components/actions/action.tsx
+++ src/components/actions/action.tsx
@@ -6,20 +6,33 @@ import type { Action } from '@/utils/page-schema'
 export type ActionProps = Action
 
 export function Action(props: ActionProps) {
-	const { type, label, href, ...rest } = props
-	switch (type) {
-		case 'link':
+	switch (props.type) {
+		case 'link': {
+			const { label, href } = props
 			return (
 				<Link href={href} className="text-foreground hover:underline hover:text-foreground/90">
 					{label}
 				</Link>
 			)
+		}
 		default:
-		case 'button':
+		case 'button': {
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			const { label, href, startIcon, type, ...rest } = props
+
+			const content = startIcon ? (
+				<>
+					<span className="material-symbols-rounded">{startIcon}</span> {label}
+				</>
+			) : (
+				label
+			)
+
 			return (
 				<Button {...rest} asChild={'href' in props}>
-					{typeof href === 'string' ? <Link href={href}>{label}</Link> : label}
+					{typeof href === 'string' ? <Link href={href}>{content}</Link> : content}
 				</Button>
 			)
+		}
 	}
 }
