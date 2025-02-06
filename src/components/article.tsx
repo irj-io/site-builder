32886ad151905a8911cb2f5057c78d43c32939f5
Brett Henderson <BrettH@m2north.com>
2025-02-06 16:30:43 +0000

    refactor: adjust help article styles

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -1,9 +1,11 @@
+import { format, parseISO } from 'date-fns'
+import { enGB } from 'date-fns/locale/en-GB'
 import Image from 'next/image'
 import { ReactNode } from 'react'
 
 import { ArticleBreadcrumb } from '@/components/article-breadcrumb'
-import { AuthorHeader } from '@/components/author-header'
 import { ArticleData } from '@/utils/post-schema'
+import { TypographyH1, TypographyLead } from './typography/typography'
 
 interface ArticleProps {
 	data: ArticleData
@@ -13,34 +15,39 @@ interface ArticleProps {
 export async function Article({ data, Markdown }: ArticleProps) {
 	return (
 		<>
-			<nav className="mb-4">
+			<nav className="mb-8">
 				<ArticleBreadcrumb slug={data.slug} />
 			</nav>
-			<div className="pb-8">
-				{data.title ? <h1 className="text-5xl font-extrabold mb-4">{data.title}</h1> : null}
-				{data.description ? (
-					<p className="text-2xl font-medium text-on-surface/50 mb-4">{data.description}</p>
-				) : null}
-				{data.author ? (
-					<AuthorHeader
-						author={{
-							avatarUrl: data.authorAvatarUrl,
-							name: data.author,
-						}}
-						date={data.date}
+			<div className="grid grid-cols-[80px_1fr] gap-6">
+				<div>
+					<Image
+						src={data.authorAvatarUrl}
+						alt={data.author}
+						className="rounded-full h-20 w-20 object-cover"
+						height={80}
+						width={80}
 					/>
-				) : null}
+				</div>
+				<div>
+					<div className="pb-8">
+						<TypographyH1>{data.title}</TypographyH1>
+						<p className="text-lg font-bold mb-4">
+							{data.author}, {format(parseISO(data.date), 'PP', { locale: enGB })}
+						</p>
+						{data.description ? <TypographyLead>{data.description}</TypographyLead> : null}
+					</div>
+					{data.banner ? (
+						<Image
+							src={data.banner}
+							alt={data.bannerAlt || ''}
+							className="max-w-full"
+							height={960}
+							width={960}
+						/>
+					) : null}
+					<div className="py-8">{Markdown}</div>
+				</div>
 			</div>
-			{data.banner ? (
-				<Image
-					src={data.banner}
-					alt={data.bannerAlt || ''}
-					className="max-w-full"
-					height={960}
-					width={960}
-				/>
-			) : null}
-			<div className="py-8">{Markdown}</div>
 		</>
 	)
 }
