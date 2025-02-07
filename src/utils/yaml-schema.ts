1ca69f4c43508a1cef2512df984eb7d40ad220b2
Brett Henderson <BrettH@m2north.com>
2025-02-07 11:46:52 +0000

    feat: add json schema for global.yaml

--- src/utils/yaml-schema.ts
+++ src/utils/yaml-schema.ts
@@ -1,10 +1,17 @@
 import { z } from 'zod'
 
 import { AnnouncementBarSchema } from '@/components/announcement-bar/announcement-bar-schema'
+import { FooterSchema } from '@/components/footers/footer-schema'
 import { HeaderSchema } from '@/components/headers/header-schema'
 import { BlocksSchema } from './page-schema'
 
-export const YamlSchema = z.object({
+export const YamlGlobalSchema = z.object({
+	announcementBar: AnnouncementBarSchema.optional().describe('AnnouncementBar properties'),
+	header: HeaderSchema.optional().describe('Header properties'),
+	footer: FooterSchema.optional().describe('Footer properties'),
+})
+
+export const YamlPageSchema = z.object({
 	announcementBar: AnnouncementBarSchema.optional().describe('Override announcementBar properties'),
 	header: HeaderSchema.pick({ theme: true }).optional().describe('Override header properties'),
 	layout: z.array(BlocksSchema).describe('A list of page blocks to add to the page'),
--- tools/build-schema.ts
+++ tools/build-schema.ts
@@ -3,16 +3,22 @@ import path from 'node:path'
 //import { inspect } from 'node:util'
 import { zodToJsonSchema } from 'zod-to-json-schema'
 
-import { YamlSchema } from '@/utils/yaml-schema'
+import { YamlGlobalSchema, YamlPageSchema } from '@/utils/yaml-schema'
 
-const jsonSchema = zodToJsonSchema(YamlSchema, 'mySchema')
+const jsonGlobalSchema = zodToJsonSchema(YamlGlobalSchema, 'yamlGlobalSchema')
+const jsonPageSchema = zodToJsonSchema(YamlPageSchema, 'yamlPageSchema')
 
 //console.debug(inspect(jsonSchema, { depth: null, colors: true }))
 
 async function main() {
 	await fs.writeFile(
+		path.join(process.cwd(), 'site-builder-global-schema.json'),
+		JSON.stringify(jsonGlobalSchema),
+		'utf-8'
+	)
+	await fs.writeFile(
 		path.join(process.cwd(), 'site-builder-page-schema.json'),
-		JSON.stringify(jsonSchema),
+		JSON.stringify(jsonPageSchema),
 		'utf-8'
 	)
 }
