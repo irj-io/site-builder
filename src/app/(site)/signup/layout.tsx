80da04d2586a5ca01e813e88d48c37ed16d588fc
Brett Henderson <BrettH@m2north.com>
2025-02-13 12:17:20 +0000

    style: restyle signup to have 2 columns

--- src/app/(site)/signup/layout.tsx
+++ src/app/(site)/signup/layout.tsx
@@ -1,14 +1,12 @@
 import PageLayout from '@/components/page-layout'
 import { Section } from '@/components/section/section'
-import { TypographyH1 } from '@/components/typography/typography'
 
 export default async function SignUpLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<PageLayout>
 			<Section>
 				<div className="container mx-auto px-6 py-16 flex flex-col items-center justify-center gap-6">
-					<TypographyH1 className="text-center mb-8">Join the flock!</TypographyH1>
-					<div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
+					{children}
 				</div>
 			</Section>
 		</PageLayout>
--- src/components/signup/signup-form.tsx
+++ src/components/signup/signup-form.tsx
@@ -95,121 +95,117 @@ export function SignUpForm({ className, ...props }: React.ComponentPropsWithoutR
 	}
 
 	return (
-		<div className={cn('flex flex-col gap-6', className)} {...props}>
-			<Card>
-				<CardHeader className="text-center">
-					{errorMessage ? (
-						<p className="text-lg text-center text-red-500 mb-4">{errorMessage}</p>
-					) : null}
+		<div className={className} {...props}>
+			{errorMessage || plan ? (
+				<div className="flex flex-col space-y-1.5 px-8 text-center">
 					{plan ? (
 						<CardTitle className="text-xl">{changeCase.sentenceCase(plan)} Plan</CardTitle>
 					) : null}
-				</CardHeader>
-				<CardContent>
-					<Form {...form}>
-						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-							<div className="grid gap-6">
-								<div className="grid gap-4">
-									<div className="grid gap-2">
-										<FormField
-											control={form.control}
-											name="fullName"
-											render={({ field }) => (
-												<FormItem>
-													<FormLabel>Your Full Name</FormLabel>
-													<FormControl>
-														<Input placeholder="Your Full Name" {...field} />
-													</FormControl>
-												</FormItem>
-											)}
-										/>
-									</div>
-									<div className="grid gap-2">
-										<FormField
-											control={form.control}
-											name="email"
-											render={({ field }) => (
-												<FormItem>
-													<FormLabel>Email Address</FormLabel>
-													<FormControl>
-														<Input placeholder="Email Address" {...field} />
-													</FormControl>
-												</FormItem>
-											)}
-										/>
-										{status === 'rejected' && error.type === 'EMAIL_TAKEN' ? (
-											<div className="flex gap-2 p-2">
-												<span>Your email already exists!</span>
-												<Link href={'https://tyto.me/login?forgot=1'}>Reset password</Link>
-												<Link href={'https://tyto.me/login'}>Login</Link>
-											</div>
-										) : null}
-									</div>
-
-									{!inviteToken ? (
-										<>
-											<div className="grid gap-2">
-												<FormField
-													control={form.control}
-													name="teamName"
-													render={({ field }) => (
-														<FormItem>
-															<FormLabel>Team Name</FormLabel>
-															<FormControl>
-																<Input placeholder="Team Name" {...field} />
-															</FormControl>
-														</FormItem>
-													)}
-												/>
-											</div>
-											<div className="grid gap-2">
-												<FormField
-													control={form.control}
-													name="teamSize"
-													render={({ field }) => (
-														<FormItem>
-															<FormLabel>Team Size</FormLabel>
-															<FormControl>
-																<Input type="number" placeholder="Team Size" {...field} />
-															</FormControl>
-														</FormItem>
-													)}
-												/>
-											</div>
-											<div className="grid gap-2">
-												<FormField
-													control={form.control}
-													name="wantsNewsletter"
-													render={({ field }) => (
-														<FormItem className="flex flex-row items-start space-x-3 space-y-0 py-4">
-															<FormControl>
-																<Checkbox checked={field.value} onCheckedChange={field.onChange} />
-															</FormControl>
-															<FormLabel>Keep me updated</FormLabel>
-														</FormItem>
-													)}
-												/>
-											</div>
-										</>
-									) : null}
-									<Button
-										type="submit"
-										className="w-full"
-										size="lg"
-										disabled={status === 'pending'}
-									>
-										{status === 'pending' ? <Loader2 className="animate-spin" /> : null}
-										Sign Up
-									</Button>
-								</div>
+					{errorMessage ? (
+						<p className="text-lg text-center text-red-500 mb-4">{errorMessage}</p>
+					) : null}
+				</div>
+			) : null}
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+					<div className="grid gap-6">
+						<div className="grid gap-4">
+							<div className="grid gap-2">
+								<FormField
+									control={form.control}
+									name="fullName"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Your Full Name</FormLabel>
+											<FormControl>
+												<Input placeholder="Your Full Name" {...field} />
+											</FormControl>
+										</FormItem>
+									)}
+								/>
 							</div>
-						</form>
-					</Form>
-				</CardContent>
-			</Card>
-			<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a:hover]:text-primary">
-				By clicking continue, you agree to our <Link href="/legal/terms">Terms of Service</Link> and{' '}
-				<Link href="/legal/privacy">Privacy Policy</Link>.
+							<div className="grid gap-2">
+								<FormField
+									control={form.control}
+									name="email"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Email Address</FormLabel>
+											<FormControl>
+												<Input placeholder="Email Address" {...field} />
+											</FormControl>
+										</FormItem>
+									)}
+								/>
+								{status === 'rejected' && error.type === 'EMAIL_TAKEN' ? (
+									<div className="flex gap-2 p-2">
+										<span>Your email already exists!</span>
+										<Link href={'https://tyto.me/login?forgot=1'}>Reset password</Link>
+										<Link href={'https://tyto.me/login'}>Login</Link>
+									</div>
+								) : null}
+							</div>
+
+							{!inviteToken ? (
+								<>
+									<div className="grid gap-2">
+										<FormField
+											control={form.control}
+											name="teamName"
+											render={({ field }) => (
+												<FormItem>
+													<FormLabel>Team Name</FormLabel>
+													<FormControl>
+														<Input placeholder="Team Name" {...field} />
+													</FormControl>
+												</FormItem>
+											)}
+										/>
+									</div>
+									<div className="grid gap-2">
+										<FormField
+											control={form.control}
+											name="teamSize"
+											render={({ field }) => (
+												<FormItem>
+													<FormLabel>Team Size</FormLabel>
+													<FormControl>
+														<Input type="number" placeholder="Team Size" {...field} />
+													</FormControl>
+												</FormItem>
+											)}
+										/>
+									</div>
+									<div className="grid gap-2">
+										<FormField
+											control={form.control}
+											name="wantsNewsletter"
+											render={({ field }) => (
+												<FormItem className="flex flex-row items-start space-x-3 space-y-0 py-4">
+													<FormControl>
+														<Checkbox checked={field.value} onCheckedChange={field.onChange} />
+													</FormControl>
+													<FormLabel>Keep me updated</FormLabel>
+												</FormItem>
+											)}
+										/>
+									</div>
+								</>
+							) : null}
+							<Button type="submit" className="w-full" size="lg" disabled={status === 'pending'}>
+								{status === 'pending' ? <Loader2 className="animate-spin" /> : null}
+								Sign Up
+							</Button>
+						</div>
+					</div>
+				</form>
+			</Form>
+			<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a:hover]:text-primary mt-6">
+				<p className="mb-2">100% privacy - we will never spam you!</p>
+				<p>
+					By clicking continue, you agree to our <Link href="/legal/terms">Terms of Service</Link>{' '}
+					and <Link href="/legal/privacy">Privacy Policy</Link>.
+				</p>
 			</div>
 		</div>
 	)
