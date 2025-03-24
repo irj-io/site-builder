17b2b34eda34f0a0c7abfb18e3c662cb16587f9d
Brett Henderson <BrettH@m2north.com>
2025-03-24 16:59:58 +0000

    feat: add improved styling for logo marquee

--- src/blocks/marquee/logo-marquee-block.tsx
+++ src/blocks/marquee/logo-marquee-block.tsx
@@ -10,18 +10,22 @@ export function LogoMarqueeBlock(props: BlockProps<LogoMarqueeProps>) {
 
 	return (
 		<Section {...getSectionProps(section)}>
-			<Marquee>
-				{items.map((item, index) => (
-					<Image
-						key={`logo-${index}`}
-						src={item.src}
-						alt=""
-						className="h-[128px] px-4 object-contain"
-						height={128}
-						width={156}
-					/>
-				))}
-			</Marquee>
+			<div className="container mx-auto max-w-3xl relative overflow-hidden">
+				<div className="absolute inset-0 bg-linear-[90deg,background,transparent_15%,transparent_85%,background] z-10" />
+
+				<Marquee>
+					{items.map((item, index) => (
+						<Image
+							key={`logo-${index}`}
+							src={item.src}
+							alt=""
+							className="h-[128px] px-4 object-contain"
+							height={128}
+							width={156}
+						/>
+					))}
+				</Marquee>
+			</div>
 		</Section>
 	)
 }
